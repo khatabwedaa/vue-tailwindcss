4411959737b70de0995efb3d7892f463ddb63095
khatabwedaa <khatabwedaa@gmail.com>
2020-05-09 16:45:08 +0200

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './src/**/*.vue',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto'],
+        'tajawal': ['Tajawal'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
